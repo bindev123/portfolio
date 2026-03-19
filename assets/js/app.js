a03e4276a9624d244b4907ce1840b47d94113608
Binaya Maharjan <binaya.maharjan.dev@gmail.com>
2026-03-19 18:12:39 +0545

    add stiky header feature

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,23 @@
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
+document.addEventListener('DOMContentLoaded', () => {
+    const navLinks = document.querySelectorAll('a[href^="#"]');
+    console.log('lkjsdflkjsdf');
 
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
+    navLinks.forEach(anchor => {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault();
+
+            navLinks.forEach(link => link.classList.remove('active'));
+
+            this.classList.add('active');
+
+            const targetId = this.getAttribute('href');
+            const targetElement = document.querySelector(targetId);
+
+            if (targetElement) {
+                targetElement.scrollIntoView({
+                    behavior: 'smooth'
+                })
+            }
         });
     });
 });
